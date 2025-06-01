31530858a3a621d9e1f68a539cadd579bcf177af
adamalfarizi <adam.alfarizi.2002@gmail.com>
2025-06-01 13:13:54 +0700

    setup tailwind

---
+++ app/(root)/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+import { PageProps } from '../../.next/types/app/layout';
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="h1-bold text-primary-100">Welcome</h1>
+    </div>
+  );
+}
