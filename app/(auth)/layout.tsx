8cf309c8031dd9a0e1dcdc093c09a10a9dc14ca4
adamalfarizi <adam.alfarizi.2002@gmail.com>
2025-10-24 23:03:28 +0700

    add todo view

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -4,7 +4,7 @@ import React, { ReactNode } from 'react';
 
 const AuthLayout = ({children}: {children: ReactNode}) => {
     return (
-        <main className='flex  min-h-screen items-center justify-center 
+        <main className='flex min-h-screen items-center justify-center 
             bg-auth-light bg-cover bg-center bg-no-repeat px-4 py-10 dark:bg-auth-dark'>
             <section className='light-border background-light800_dark200 shadow-light-100_dark100 
                 rounded-[10px] border px-4 py-10 shadow-md sm:min-w=[520px] sm:px-8'> 
